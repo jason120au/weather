575698a01c192a945c764cfdecb87c7e3d55c76f
Jason Campbell <jasonc@jason120au.net>
2021-05-15 15:23:14 +1000

    add highlights to autocomplete

--- src/components/autocomplete.js
+++ src/components/autocomplete.js
@@ -1,7 +1,11 @@
 // server.autosuggest.js
 import React from 'react';
 import Autosuggest from 'react-autosuggest';
+
 import './styles.css';
+import AutosuggestHighlightMatch from "autosuggest-highlight/umd/match";
+import AutosuggestHighlightParse from "autosuggest-highlight/umd/parse";
+
 
 class Autocomplete extends React.Component {
     constructor() {
@@ -25,14 +29,14 @@ class Autocomplete extends React.Component {
     // Trigger suggestions
     getSuggestionValue = suggestion => suggestion.text;
 
-    // Render Each Option
-    renderSuggestion = suggestion => (
-        <span className="sugg-option">
-            <span className="name">
-                {suggestion.text}
-            </span>
-        </span>
-    );
+    // // Render Each Option
+    // renderSuggestion = suggestion => (
+    //     <span className="sugg-option">
+    //         <span className="name">
+    //             {suggestion.text}
+    //         </span>
+    //     </span>
+    // );
 
     // OnChange event handler
     onChange = (event, { newValue }) => {
@@ -64,6 +68,35 @@ class Autocomplete extends React.Component {
             suggestions: []
         });
     };
+
+    getSuggestionValue(suggestion) {
+        return `${suggestion.text}`;
+      }
+      
+      renderSuggestion(suggestion, { query }) {
+        const suggestionText = `${suggestion.text}`;
+        const matches = AutosuggestHighlightMatch(suggestionText, query);
+        const parts = AutosuggestHighlightParse(suggestionText, matches);
+      
+        return (
+          <span className={'suggestion-content ' + suggestion.twitter}>
+            <span className="name">
+              {
+                parts.map((part, index) => {
+                  const className = part.highlight ? 'highlight' : null;
+      
+                  return (
+                    <span className={className} key={index}>{part.text}</span>
+                  );
+                })
+              }
+            </span>
+          </span>
+        );
+      }
+
+
+
 
     render() {
         const { value, suggestions } = this.state;
